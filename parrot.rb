6dd64a760e554a52749970bd7892499694333d43
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-04-17 19:26:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase)
+  phrase = "Squawk!"
+  puts "Squawk!"
+  return parrot
+end
+
+def parrot(phrase)
+  phrase = "Pretty bird!"
+  puts phrase
+  return phrase
+end
